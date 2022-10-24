fd35117f72852449ec3f3b080556dea34c373c41
Damir Sh. Yakupov <yds@bk.ru>
2022-10-25 03:23:10 +0400

    feat: add INC and DEC events

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * $Git: index.ts v0.2.0-8-4 2022-10-25 03:17:29+04:00 toor $
+ * $Git: index.ts v0.2.1-13-1 2022-10-25 03:22:31+04:00 toor $
  */
 
 import {Observable} from "./observable";
@@ -13,4 +13,9 @@ const obs2 = new Observer(42);
 stream$.subscribe(obs1);
 stream$.subscribe(obs2);
 
+console.log(`observer1 ${obs1.getState()}, observer2 ${obs2.getState()}`);
+
+stream$.update({type: 'INC'});
+console.log(`observer1 ${obs1.getState()}, observer2 ${obs2.getState()}`);
+stream$.update({type: 'DEC'});
 console.log(`observer1 ${obs1.getState()}, observer2 ${obs2.getState()}`);
