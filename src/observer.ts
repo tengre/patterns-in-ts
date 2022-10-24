759a59ce6e0c3e9f1ee288a2eb5e6ecdee2d416d
Damir Sh. Yakupov <yds@bk.ru>
2022-10-25 02:48:09 +0400

    feat(custom): add class Observable

--- src/observer.ts
+++ src/observer.ts
@@ -1,25 +1,41 @@
 /**
- *  $Git: observer.ts v0.1.1-3-2 2022-10-25 01:13:13+04:00 toor $
+ * $Git: observer.ts v0.1.1-3-4 2022-10-25 02:46:00+04:00 toor $
  */
-class Observable {
-    protected observers: Observer[];
-    constructor() {
-        this.observers = [];
+import {Action} from "./types";
+
+export class Observer {
+    protected state: number;
+    protected initialState: number;
+    constructor(state: number) {
+        this.state = state;
+        this.initialState = state;
     }
 
-    subscribe(observer: Observer) {
-        this.observers.push(observer);
+    getState() {
+        return this.state;
     }
 
-    unsubscribe(observer: Observer) {
-        this.observers = this.observers.filter(item => item !== observer);
+    getInitialState() {
+        return this.initialState;
     }
 
     update(action: Action) {
-        this.observers.forEach(
-            observer => {
-                observer.update(action)
+        switch(action.type) {
+            case 'INC':
+                this.state = ++this.state;
+                break;
+            case 'DEC':
+                this.state = --this.state;
+                break;
+            case 'ADD':
+                this.state = this.state + (action?.payload ?? 0);
+                break;
+            case 'SUB':
+                this.state = this.state - (action?.payload ?? 0);
+                break;
+            default: {
+                this.state = this.initialState;
             }
-        )
+        }
     }
 }
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+/**
+ * $Git: types.ts v0.1.1-3-3 2022-10-25 02:41:29+04:00 toor $
+ */
+export type Action = {type: string, payload?: number};
