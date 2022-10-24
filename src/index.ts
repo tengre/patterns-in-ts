f2bac77c2055a5f09bff53e8157bb3035fafc76c
Damir Sh. Yakupov <yds@bk.ru>
2022-10-25 03:18:47 +0400

    feat: subscribe observable object to observers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 /**
- * $Git: index.ts v0.2.0-8-2 2022-10-25 03:04:35+04:00 toor $
+ * $Git: index.ts v0.2.0-8-4 2022-10-25 03:17:29+04:00 toor $
  */
 
 import {Observable} from "./observable";
 import {Observer} from "./observer";
 
 const stream$ = new Observable();
-const obs1 = new Observer(1);
 
-console.log(`observer1 ${obs1.getState()}`);
+const obs1 = new Observer(1);
+const obs2 = new Observer(42);
+
+stream$.subscribe(obs1);
+stream$.subscribe(obs2);
+
+console.log(`observer1 ${obs1.getState()}, observer2 ${obs2.getState()}`);
